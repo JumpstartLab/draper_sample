f54b3f224e3206ee74341d80cb95f8bc4182f8e4
Jeff Casimir <jeff@casimircreative.com>
2011-07-07 12:49:06 -0400

    Completed basic UI for store

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,9 +1,11 @@
 module ProductsHelper
-  def print_stock(quantity)
-    if quantity > 0
-      content_tag :span, "In Stock (#{quantity})", :class => "in_stock"
+  def print_stock(stock, requested = 1)
+    if stock >= requested
+      content_tag :span, "In Stock (#{stock})", :class => "in_stock"
+    elsif stock > 0
+      content_tag :span, "Insufficient Stock (only #{stock})", :class => "low_stock"      
     else
-      content_tag :span, "Out of Stock", :class => "out_stock"      
+      content_tag :span, "Out of Stock", :class => "out_stock"            
     end
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Jsmerchant::Application.routes.draw do
   resources :orders, :products
   resources :order_items, :only => [:create, :edit, :update, :destroy]
+  
+  root :to => "products#index"
 end
